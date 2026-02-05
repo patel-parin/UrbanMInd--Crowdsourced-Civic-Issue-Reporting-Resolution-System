e436c932a7003d8b195b276d692516cd9962d8c8
Arman2337 <23it119@charusat.edu.in>
2026-02-05 22:20:26 +0530

    add all routes for worki best mobile app

--- mobile/lib/routes/app_routes.dart
+++ mobile/lib/routes/app_routes.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import '../features/auth/screens/login_screen.dart';
-import '../features/auth/screens/signup_screen.dart';
+import '../features/auth/screens/register_screen.dart';
 import '../features/auth/screens/splash_screen.dart';
 import '../features/home/screens/home_screen.dart';
-import '../features/home/screens/report_issue_screen.dart';
-import '../features/home/screens/issue_detail_screen.dart';
+import '../features/citizen/screens/report_issue_screen.dart';
+import '../features/citizen/screens/my_issues_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
-  static const String signup = '/signup';
-  static const String home = '/home';
+  static const String register = '/register';
+  static const String dashboard = '/dashboard';
   static const String reportIssue = '/report-issue';
-  static const String issueDetail = '/issue-detail';
+  static const String myIssues = '/my-issues';
 
   static Map<String, WidgetBuilder> get routes => {
         splash: (context) => const SplashScreen(),
         login: (context) => const LoginScreen(),
-        signup: (context) => const SignupScreen(),
-        home: (context) => const HomeScreen(),
+        register: (context) => const RegisterScreen(),
+        dashboard: (context) => const HomeScreen(),
         reportIssue: (context) => const ReportIssueScreen(),
-        issueDetail: (context) => const IssueDetailScreen(),
+        myIssues: (context) => const MyIssuesScreen(),
       };
 }
