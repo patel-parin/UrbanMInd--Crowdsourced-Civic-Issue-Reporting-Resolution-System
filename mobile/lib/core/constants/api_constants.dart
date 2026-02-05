9f7eddf336afeda6c23d6c6634315af68480c1bc
Arman2337 <23it119@charusat.edu.in>
2026-02-05 22:15:48 +0530

    update api constants

--- mobile/lib/core/constants/api_constants.dart
+++ mobile/lib/core/constants/api_constants.dart
@@ -1,11 +1,14 @@
 class ApiConstants {
   // Use 10.0.2.2 for Android Emulator to access localhost
+  // Use localhost for iOS Simulator
+  // Use your machine's IP for physical device
   static const String baseUrl = 'http://10.111.27.165:5000/api';
 
-  static const String login = '$baseUrl/auth/login';
-  static const String register = '$baseUrl/auth/register';
-  static const String me = '$baseUrl/auth/me';
+  static const String login = '/auth/login';
+  static const String register = '/auth/register';
+  static const String me = '/auth/me';
 
-  static const String createIssue = '$baseUrl/issue/create';
-  static const String myIssues = '$baseUrl/issue/my-issues';
+  static const String createIssue = '/issue/create';
+  static const String myIssues = '/issue/my-issues';
+  static const String allIssues = '/issue/all';
 }
